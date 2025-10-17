07eee3bde4384511eb2ad3b92aa9445cbbb2a722
Jay Joshi <joshijayc075@gmail.com>
2025-10-17 11:11:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from '@/components/mode-toggle.tsx';
+import React from 'react'
+
+const App = () =>{
+    return (
+        <div>
+            <div className="text-9xl font-bold">App</div>
+            <ModeToggle />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,22 @@
+import { Moon, Sun } from "lucide-react"
+import { Button } from "@/components/components/ui/button"
+import { useTheme } from "@/components/theme-provider"
+
+export function ModeToggle() {
+    const { theme, setTheme } = useTheme()
+
+    const toggleTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light")
+    }
+
+    return (
+        <Button 
+            variant="icon"
+            size="icon"
+            onClick={toggleTheme} className={undefined}        >
+            <Moon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+            <Sun className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+            <span className="sr-only">Toggle theme</span>
+        </Button>
+    )
+}
